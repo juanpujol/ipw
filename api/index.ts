562f5ae1b892b4b5c0941d85f79a44d4866a40b3
Juan Pujol <jp@beakyn.com>
2020-08-15 07:42:02 -0300

    fix(api): rename clientIp prop to just ip

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import { NowRequest, NowResponse } from '@vercel/node'
 import * as requestIp from 'request-ip';
 
 export default async (req: NowRequest, res: NowResponse) => {
-  const clientIp = requestIp.getClientIp(req);
+  const ip = requestIp.getClientIp(req);
 
-  return res.status(200).json({ clientIp })
+  return res.status(200).json({ ip })
 }
